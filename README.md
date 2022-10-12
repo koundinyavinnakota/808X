23c5cb83f247c4319665224ab09bfe3244bfbcfd
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-10-12 18:39:43 -0400

    Final push

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
-# PID_Controller
-- A program that uses PID controller to control output velocity.
----
-Part - 1:
-- Driver : Koundinya Vinnakota
-- Navigator : Mayank Sharma
+# Valgrind Excercise
+- This program is to understand the functionality of Valgrind
 
-Part - 2:
-- Driver : Aneesh Chodisetty
-- Navigator: Aniruddh Balram
 
 ## Dependencies
 - C++ 
@@ -23,16 +16,5 @@ mkdir build
 cd build
 cmake ..
 make
-Run program: ./app/PID
+Run program: ./app/Checker
 ```
-## Steps to generate LCOV report
-```
-cd <project_directory>
-mkdir build
-cd build
-cmake -D COVERAGE=ON -D CMAKE_BUILD_TYPE=Debug ../
-make
-make code_coverage
-```
-## Class Diagram 
-![Class Diagram](Class_diagram.png "Class Diagram")
--- app/main.cpp
+++ app/main.cpp
@@ -1,36 +1,22 @@
 /**
- * @Driver - Koundinya Vinnakota
- * @Navigator - Mayank Sharma
+ * @author - Koundinya Vinnakota
  * @version - 0.1
  * @copyright - 2022 - vkd@umd.edu
  * @return int - 0, if there is no error in the code.
  */
-#include <iostream>
-#include <pid.hpp>
-
+// #include "GradeCalculator.cpp"
+#include<iostream>
+#include <vector>
 int main() {
-  // Parameters
-  double k_p{0.1};
-  double k_d{0.01};
-  double k_i{0.5};
-  double dt{0.1};
-  double max{50};
-  double min{-50};
+   
 
-  // Object initialization for the class PID
-  PID controller_1(k_p, k_d, k_i, dt, max, min);
+    std::vector<int> v { 1, 2, 3, 4, 5 };
+    v[4] = 0;
 
-  // Displaying the computed velocity to the user
-  double process_value{20};
-  double set_point{10};
-  double current_difference{0};
-  double total_time{10};
+    bool condition = true;
+    if (condition) {
+    // Do the thing
+    std::cout << " Hi " << std::endl;
+    }
 
-  for (int time_step = 0; time_step < static_cast<int>(total_time / dt);
-       time_step++) {
-    current_difference = controller_1.ComputeVelocity(process_value, set_point);
-    std::cout << "The current velocity is: " << process_value << std::endl;
-    process_value += current_difference;
-  }
-  return 0;
 }
